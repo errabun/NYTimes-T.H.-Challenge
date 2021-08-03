e4683877ec461fe90b2f4cd936329400078f607b
Eric Rabun <errabun@gmail.com>
2021-08-03 10:15:52 -0600

    Add onChange property to select menu

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -8,12 +8,12 @@ function Dashboard () {
 
   const handleClick = (e) => {
     setNewsSelection(e.target.value)
-    return fetch(`https://api.nytimes.com/svc/topstories/v2/${e.target.value}.json?api-key=${apiKey}`)
+    return fetch(`https://api.nytimes.com/svc/topstories/v2/${newsSelection}.json?api-key=${apiKey}`)
       .then(resp => {
         if (!resp.ok) {
           throw new Error('Couldn\'t find any articles, please try again')
         } else {
-          return resp.json()
+          return console.log(resp.json())
         }
       })
   }
@@ -21,35 +21,26 @@ function Dashboard () {
   return(
     <main>
       <h2>What kind of articles would you like to view?</h2>
-      <select name='article-select' id='article-select' value={newsSelection} onChange={setNewsSelection(newsSelection)}>
+      <select name='article-select' id='article-select' value={newsSelection} onChange={e => setNewsSelection(e.target.value)}>
         <option value=''> --Please select an option--</option>
         <option value='arts'>Arts</option>
-        <option value='automobiles'>Automobiles</option>
         <option value='books'>Books</option>
         <option value='business'>Business</option>
         <option value='fashion'>Fashion</option>
         <option value='food'>Food</option>
         <option value='health'>Health</option>
-        <option value='home'>Home</option>
-        <option value='insider'>Insider</option>
         <option value='magazine'>Magazine</option>
         <option value='movies'>Movies</option>
-        <option value='nyregion'>New York Region</option>
-        <option value='obituaries'>Obituaries</option>
         <option value='opinion'>Opinion</option>
         <option value='politics'>Politics</option>
         <option value='realestate'>Real Estate</option>
         <option value='science'>Science</option>
-        <option value='sundayreview'>Sunday Review</option>
         <option value='technology'>Technology</option>
-        <option value='theater'>Theater</option>
-        <option value='t-magazine'>T-Magazine</option>
         <option value='travel'>Travel</option>
-        <option value='upshot'>Upshot</option>
         <option value='us'>US News</option>
         <option value='world'>World News</option>
       </select>
-      <button onClick={e => handleClick}>Show me news</button>
+      <button onClick={e => handleClick(e)}>Show me news</button>
     </main>
   )
 }
