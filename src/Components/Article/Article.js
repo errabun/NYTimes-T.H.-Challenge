d7a6884bd5fa0f2a2c9ebf8a74f3acb053708ce9
Eric Rabun <errabun@gmail.com>
2021-08-05 16:18:09 -0600

    Responsive styling for Article component

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -7,13 +7,16 @@ function Article({ selectedArticle }) {
   const formatDate = published_date.slice(0, 10).toString()
 
   return (
-    <div>
-      <h2>{title}</h2>
-      <h4>{byline}</h4>
-      <p>Published on: {formatDate}</p>
-      <img src={multimedia[0].url} alt={multimedia[0].caption} height='350' width='600'/>
-      <p>{abstract}</p>
-      <a href={url}>Take me to this article</a>
+    <div className='article-container'>
+      <h1 className='title'>{title}</h1>
+      <h4>{byline} <span>Published on: {formatDate}</span></h4>
+      <div className='img-container'>
+        <img src={multimedia[0].url} alt={multimedia[0].caption} height='350' width='600'/>
+        <div className='abstract-container'>
+          <p>{abstract}</p>
+          <a href={url}>Take me to this article</a>
+        </div>
+      </div>
     </div>
   )
 }
